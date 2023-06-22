e11525a0e28665870a374ef91d4888521cfdd855
Ronald David <ronaldcriolo02@gmail.com>
2023-06-22 00:27:38 -0300

    Atualizando arquivos

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Site_E-nature
-Projeto de "Desenvolvimento de Aplicações para Internet" da Faculdade, onde o proposito dessa atividade era construir um site responsivo e com alguns elemento visuais.
+<h1 align="center">
+    <img src="https://i.ibb.co/YTkYz4g/Logo-E-nature.png">
+</h1>
+
+<h2 align="center">
+    <img src="https://i.ibb.co/pjrdsZf/e-nature-min-1.gif"><br><br>
+</h2><br>
+
+<h3 align="center"> 
+  
+  [Access project](https://enature.netlify.app/) 
+</h3><br>
+
+## ⭐ About
+
+Projeto de **Desenvolvimento de Aplicações para Internet** da Faculdade, onde o proposito dessa atividade era construir um site responsivo e com alguns elemento visuais. É para isso o tema escolhido foi `natureza`.
+
+<br>
+
+## 🧰 Languages and Tools
+
+-   JavaScript
+-   HTML e CSS
+-   [Bootstrap](https://getbootstrap.com.br/docs/4.1/getting-started/introduction/)
---
+++ src/assets/js/main.js
@@ -0,0 +1,37 @@
+// Declarando Variaveis
+var toggleModal = document.querySelectorAll(".toggle-modal");
+var toggleMenu = document.querySelectorAll(".toggle-menu");
+var menuMobile = document.querySelector(".navBar-mobile");
+var btnMenuMobImg = document.querySelector(".btn-menu-mob ion-icon"); 
+
+//Abrindo e Fechado Modal Mensagem
+for (i = 0; i < toggleModal.length; i++) {
+    toggleModal[i].addEventListener("click", function () {
+        var modalMensagem = document.querySelector(".field-modal");
+        var overlay = document.querySelector(".overlay");
+
+            overlay.classList.toggle("open");
+            modalMensagem.classList.toggle("open");
+            modalMensagem.classList.toggle("slider-top-in");
+    });
+}
+
+//Abrindo e Fechado Menu Mobile
+for (m = 0; m < toggleMenu.length; m++) {
+    toggleMenu[m].addEventListener("click", function () {
+        var overlay = document.querySelector(".menu-overlay");
+
+            overlay.classList.toggle("open");
+            menuMobile.classList.toggle("close-menu");
+            menuMobile.classList.toggle("open-menu");
+
+            var icon = btnMenuMobImg.getAttribute("name"); 
+            
+            if (icon === "menu-outline") {
+                btnMenuMobImg.setAttribute("name", "close-outline");
+            } else{
+                btnMenuMobImg.setAttribute("name", "menu-outline");
+            }
+    });
+}
+
