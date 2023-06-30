a4a2df8eea54712de56e45468f136e870341c0e4
Ronald David <ronaldcriolo02@gmail.com>
2023-06-30 18:14:25 -0300

    Organizando espaçamento

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,7 +2,7 @@
 var toggleModal = document.querySelectorAll(".toggle-modal");
 var toggleMenu = document.querySelectorAll(".toggle-menu");
 var menuMobile = document.querySelector(".navBar-mobile");
-var btnMenuMobImg = document.querySelector(".btn-menu-mob ion-icon"); 
+var btnMenuMobImg = document.querySelector(".btn-menu-mob ion-icon");
 
 //Abrindo e Fechado Modal Mensagem
 for (i = 0; i < toggleModal.length; i++) {
@@ -10,29 +10,27 @@ for (i = 0; i < toggleModal.length; i++) {
         var modalMensagem = document.querySelector(".field-modal");
         var overlay = document.querySelector(".overlay");
 
-            overlay.classList.toggle("open");
-            modalMensagem.classList.toggle("open");
-            modalMensagem.classList.toggle("slider-top-in");
+        overlay.classList.toggle("open");
+        modalMensagem.classList.toggle("open");
+        modalMensagem.classList.toggle("slider-top-in");
     });
 }
-
 
 //Abrindo e Fechado Menu Mobile
 for (m = 0; m < toggleMenu.length; m++) {
     toggleMenu[m].addEventListener("click", function () {
         var overlay = document.querySelector(".menu-overlay");
 
-            overlay.classList.toggle("open");
-            menuMobile.classList.toggle("close-menu");
-            menuMobile.classList.toggle("open-menu");
+        overlay.classList.toggle("open");
+        menuMobile.classList.toggle("close-menu");
+        menuMobile.classList.toggle("open-menu");
 
-            var icon = btnMenuMobImg.getAttribute("name"); 
-            
-            if (icon === "menu-outline") {
-                btnMenuMobImg.setAttribute("name", "close-outline");
-            } else{
-                btnMenuMobImg.setAttribute("name", "menu-outline");
-            }
+        var icon = btnMenuMobImg.getAttribute("name");
+
+        if (icon === "menu-outline") {
+            btnMenuMobImg.setAttribute("name", "close-outline");
+        } else {
+            btnMenuMobImg.setAttribute("name", "menu-outline");
+        }
     });
 }
-
